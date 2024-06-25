4a14d3a375ecbf5e7032e7e4f79f5720a50b876b
TPKapgate <101078470+TPKapgate@users.noreply.github.com>
2024-06-25 19:04:08 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,31 @@
-// db.js
-import mongoose from "mongoose";
-const url = process.env.MONGODB || "0.0.0.0:27017";
+import { MongoClient } from "mongodb";
+
+const uri = "mongodb+srv://2017ugpi006:TFHf3Ajdm0HNuUBX@cluster0.irncr8i.mongodb.net/issueTrack?retryWrites=true&w=majority";
+
+let client;
+
 export const connect = async () => {
   try {
-    await mongoose.connect(`mongodb://${url}/issueTrack`);
-    console.log("Database connected successfully");
+    client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    await client.connect();
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("Database connection error:", error);
+    console.error("MongoDB connection error:", error);
+  }
+};
+
+export const getClient = () => client;
+
+// Example function to close the MongoDB connection
+export const closeConnection = async () => {
+  try {
+    await client.close();
+    console.log("MongoDB connection closed");
+  } catch (error) {
+    console.error("Error closing MongoDB connection:", error);
   }
 };
