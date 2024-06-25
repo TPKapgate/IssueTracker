5adda3c7de53527f959e08673974bb6ca952376a
TPKapgate <101078470+TPKapgate@users.noreply.github.com>
2024-06-25 15:50:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen('IssueTracker', () => {
   db();
   console.log(`Server running on port ${PORT}`);
 });
