f0b42177389f37cc055e68696f03c0e47a24a039
TPKapgate <101078470+TPKapgate@users.noreply.github.com>
2024-06-25 17:26:30 +0530

    Update issueRoutes.js

--- issueRoutes.js
+++ issueRoutes.js
@@ -4,6 +4,7 @@ import { getIssues, getCreateIssueForm, createIssue } from './issueController.js
 
 const router = express.Router();
 
+
 router.get('/:projectId', getIssues);
 router.get('/create/:projectId', getCreateIssueForm);
 router.post('/create/:projectId', createIssue);
