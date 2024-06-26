a2ead1d761fd1e6adc5b3459a33f4477496abd31
TPKapgate <101078470+TPKapgate@users.noreply.github.com>
2024-06-26 12:48:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
