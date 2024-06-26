cb35c4e8e97d100f32127a253f374b14a0931a30
TPKapgate <101078470+TPKapgate@users.noreply.github.com>
2024-06-26 12:46:32 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,22 @@
+// db.js
+// import mongoose from "mongoose";
+// const url = "mongodb+srv://2017ugpi006:TFHf3Ajdm0HNuUBX@cluster0.irncr8i.mongodb.net/issueTrack?retryWrites=true&w=majority";
+
+// export const connect = async () => {
+//   try {
+//     await mongoose.connect(`mongodb://${url}/issueTrack`);
+//     console.log("Database connected successfully");
+//   } catch (error) {
+//     console.error("Database connection error:", error);
+//   }
+// };
+// mongodb://localhost:27017/
+
+
+
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb+srv://2017ugpi006:TFHf3Ajdm0HNuUBX@cluster0.irncr8i.mongodb.net/issueTrack?retryWrites=true&w=majority";
+const uri = "mongodb+srv://2017ugpi006:TFHf3Ajdm0HNuUBX@cluster0.irncr8i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 let client;
 
