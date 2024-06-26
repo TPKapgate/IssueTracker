2d50cf7e0ad494046916db0baa557d90b8e3831b
TPKapgate <101078470+TPKapgate@users.noreply.github.com>
2024-06-26 12:48:01 +0530

    Update Project.js

--- Project.js
+++ Project.js
@@ -1,9 +1,29 @@
-import mongoose from 'mongoose';
+import { getClient } from './db.js';
 
-const projectSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    author: { type: String, required: true }
-});
+const dbName = "issueTrack";
+const collectionName = "projects";
 
-export const Project = mongoose.model('Project', projectSchema);
+export const Project = {
+  create: async (project) => {
+    const client = getClient();
+    const collection = client.db(dbName).collection(collectionName);
+    const result = await collection.insertOne(project);
+    return result;
+  },
+
+  find: async (query = {}) => {
+    const client = getClient();
+    const collection = client.db(dbName).collection(collectionName);
+    const projects = await collection.find(query).toArray();
+    return projects;
+  },
+
+  findById: async (id) => {
+    const client = getClient();
+    const collection = client.db(dbName).collection(collectionName);
+    const project = await collection.findOne({ _id: new ObjectId(id) });
+    return project;
+  },
+
+  // Add other CRUD operations as needed
+};
